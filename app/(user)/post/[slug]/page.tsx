859532591b47aef28cd58d4b1a132b947516035d
Frank Santos <fransecre@gmail.com>
2023-07-09 22:50:44 +0200

    Article Page

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -1,17 +1,17 @@
-import { client } from '@/lib/sanity.client';
-import urlFor from '@/lib/urlFor';
-import { groq } from 'next-sanity';
-import Image from 'next/image';
-import React from 'react'
+import { client } from "@/lib/sanity.client";
+import urlFor from "@/lib/urlFor";
+import { groq } from "next-sanity";
+import Image from "next/image";
+import React from "react";
 
-type Props ={
-    params: {
-        slug: string
-    }
-}
+type Props = {
+  params: {
+    slug: string;
+  };
+};
 
-async function Post({params: {slug}}: Props) {
-    const query = groq`
+async function Post({ params: { slug } }: Props) {
+  const query = groq`
     *[_type=='post' && slug.current ==$slug][0]{
         ...,
         author->,
@@ -19,40 +19,65 @@ async function Post({params: {slug}}: Props) {
     }
     `;
 
-    const post: Post = await client.fetch(query, {slug})
+  const post: Post = await client.fetch(query, { slug });
   return (
-    <article className='px-10 pb-28'>
-        <section className='space-y-2 border-[#F7AB0A] text-white'>
-            <div className='relative min-h-56 flex flex-col md:flex-row justify-between'>
-                <div className='absolute top-0 w-full h-full opacity-10 blur-sm p-10'>
-                <Image
-                className="object-cover object-center mx-auto"
-                src={urlFor(post.mainImage).url()}
-                alt={post.author.name}
-                fill
-              />
-                </div>
+    <article className="px-10 pb-28">
+      <section className="space-y-2 border border-[#F7AB0A] text-white">
+        <div className="relative min-h-56 flex flex-col md:flex-row justify-between">
+          <div className="absolute top-0 w-full h-full opacity-10 blur-sm p-10">
+            <Image
+              className="object-cover object-center mx-auto"
+              src={urlFor(post.mainImage).url()}
+              alt={post.author.name}
+              fill
+            />
+          </div>
 
-                <section>
-                    <div>
-                        <div>
-                            <h1 className='text-4xl font-extrabold'>
-                                {post.title}
-                            </h1>
-                            <p>
-                    {new Date(post._createdAt).toLocaleDateString("en-US", {
-                      day: "numeric",
-                      month: "long",
-                      year: "numeric",
-                    })}
-                  </p>
-                        </div>
-                    </div>
-                </section>
+          <section className="p-5 w-full bg-[#F7AB0A]" >
+            <div className="flex flex-col md:flex-row justify-between gap-y-5">
+              <div >
+                <h1 className="text-4xl font-extrabold">{post.title}</h1>
+
+                <p>
+                  {new Date(post._createdAt).toLocaleDateString("en-US", {
+                    day: "numeric",
+                    month: "long",
+                    year: "numeric",
+                  })}
+                </p>
+              </div>
+
+              <div className="flex items-center space-x-2">
+                <Image
+                  className="rounded-full"
+                  src={urlFor(post.author.image).url()}
+                  alt={post.author.name}
+                  height={40}
+                  width={40}
+                />
+
+                <div className="w-64">
+                  <h3 className="text-lg font-bold">{post.author.name}</h3>
+                  <div>{/**INFO */}</div>
+                </div>
+              </div>
+
+              <div>
+                <h2 className="italic pt-10">{post.description}</h2>
+                <div className="flex items-center justify-end mt-auto space-x-2">
+                    {post.categories.map((category)=>(
+                        <p key={category._id} className="bg-gray-800 text-white px-3 py-1 rounded-full text-sm font-semibold mt-4">
+                            {category.title}
+                        </p>
+                    ))}
+                </div>
+              </div>
             </div>
-        </section>
+          </section>
+        </div>
+      </section>
     </article>
-  )
+  );
 }
 
-export default Post
+export default Post;
