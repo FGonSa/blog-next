73b98f76ab2e3ca8f3c0ad47777adad044ca401a
Frank Santos <fransecre@gmail.com>
2023-07-09 17:41:01 +0200

    Article Page Init

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -14,7 +14,6 @@ export default async function userPage() {
   const posts = await client.fetch(query);
 return(
    <main className="text-center p-2">
-    User
     <BlogList posts={posts} />
    </main>
     
--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -5,7 +5,7 @@ function Banner() {
     <div className='flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10'>
         <div >
             <h1 className='text-7xl'>Franky's Next Blog</h1>
-            <h2 className='mt-5 md:mt-0'>Welcome to my Blog</h2>
+            <h2 className='mt-5 md:mt-0'>Eslógan</h2>
         </div>
         <p className='mt-5 md:mt-2 text-gray-400 max-w-sm'>Un poco de texto</p>
     </div>
